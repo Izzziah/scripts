66c682311d33487f47c9a87d52786f04807be1ad
isgrambo <igrambo@accelitec.com>
2020-01-14 14:43:23 -0800

    new script for search rpl files for query params

---
+++ searchRplQueryParams.ps1
@@ -0,0 +1,33 @@
+param(
+    [Parameter(ValueFromPipeline=$true)][System.IO.FileInfo[]]$files
+    ,[System.String]$path='.'
+    ,[System.String]$searchString
+)
+echo "Searching for: $searchString`n`n"
+$xml = new-object -typename xml; 
+
+ls -Path $path -filter *.rdl | % {
+    $writeTitle = $true;
+    $title = $_.Name
+    $xml.load($_.fullname); 
+    $tbl = $xml.Report.DataSets.DataSet.Query; 
+    $tbl | % { 
+        $foundParam = $_.QueryParameters | select -ExpandProperty queryparameter  | select name  | sls $searchString
+        if ($foundParam -ne $null) {
+            if ($writeTitle) {
+                $writeTitle = $false;
+                write-output "> $title"; 
+            }
+            if ($_.CommandType -ne $null) {
+                # storedProc?
+                echo "`t###$($_.CommandType): $($_.CommandText)";
+                # echo "`tfound param: '$foundParam'";
+            } else {
+                # non-storedProc
+                echo "`t###Query: `n`t$($_.CommandText -replace '\s+',' ' -replace "from","`n`t`tfrom" -replace "where","`n`t where")";
+                # echo "`tfound param: '$foundParam'";
+            }
+            echo "`t`t**found param**: '$foundParam'";
+        }
+    }
+}
